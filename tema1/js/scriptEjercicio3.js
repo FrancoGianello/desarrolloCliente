f14e6267c793ec16d00cdf2c5a3d6ac6967624cc
Franco <francoacon98@gmail.com>
2022-10-23 23:26:30 +0200

    actualizacion ejercicio

--- tema1/js/scriptEjercicio3.js
+++ tema1/js/scriptEjercicio3.js
@@ -1,3 +1,4 @@
+/*
 function perimetro(){
    let lado1= document.getElementsByClassName("lado")[0].value;
    let lado2= document.getElementsByClassName("lado")[1].value;
@@ -18,11 +19,43 @@ function perimetro(){
    fila.appendChild(casilla).innerHTML= parseInt(lado1) + parseInt(lado2);
 
 }
-
+*/
+function mostrarHoraDetallada(){
+   let hora = new Date();
+   let mostrar = hora.toLocaleString().split(",");
+   document.getElementById('generar').innerHTML = mostrar[1];
+   //setInterval(mostrarHoraDetallada, 60);  
+}
+function mostrarHoraSimplificada(){
+   let hora = new Date();
+   let mostrar = hora.getHours()+ ":"+ hora.getMinutes()+ " AM";
+   if(hora.getHours()>12) mostrar = (hora.getHours()-12)+ ":"+ hora.getMinutes()+ " PM";
+   document.getElementById('generar').innerHTML = mostrar;
+   //setInterval(mostrarHoraSimplificada, 60); 
+}
 function quedanDias(){
    let fecha = new Date();
    let fechaFinal = new Date(2023,0,1);
    let contenedor = document.getElementById('ejer2');
    let quedan = fechaFinal-fecha;
    contenedor.innerHTML = "<p>Quedan: "+parseInt(quedan/86400000)+ " días para el fin</p>";
+}
+function generarDatos(){
+   let pelicula = document.getElementsByClassName("datos")[0].value;
+   let autor = document.getElementsByClassName("datos")[1].value;
+   let anno = document.getElementsByClassName("datos")[2].value;
+   let textoPelicula = pelicula.split(" ").join("");
+   let textoAutor = autor.split(" ").join("");
+   let lista = document.createElement("ul");
+   document.getElementById("ejer3").innerHTML="";
+   document.getElementById("ejer3").appendChild(lista);
+   let listados = document.createElement("li");
+   lista.appendChild(listados);
+   listados.innerHTML = "Longitud de: <ul class='datosIntroducidos'><li>"+ pelicula + " es "+ textoPelicula.length + " </li> <li>"+ autor +" es "+ textoAutor.length+"</li> </ul>";
+   listados = document.createElement("li");
+   lista.appendChild(listados);
+   listados.innerHTML = "Datos formateados: <ul class='datosIntroducidos'><li>"+pelicula.toUpperCase() + "</li> <li>" + autor.toUpperCase() + "</li> <li>"+ pelicula.toLowerCase() + "</li> <li>"+ pelicula.toLowerCase()+"</li> </ul>";
+   listados = document.createElement("li");
+   lista.appendChild(listados);
+   listados.innerHTML = "Datos introducidos: <ul class='datosIntroducidos'><li>"+pelicula+"</li> <li>"+autor+"</li> <li>"+anno+"</li> </ul>";
 }
